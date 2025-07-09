7d36e2a46de6b191af2af884213be9d0429240f5
abh3shek <abhishekganapure22@gmail.com>
2025-07-09 17:28:17 +0530

    :/

--- public/chatroom.php
+++ public/chatroom.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 require_once __DIR__ . '/../includes/session.php';
 require_once __DIR__ . '/../includes/db.php';
 require_once __DIR__ . '/../includes/auth.php';
